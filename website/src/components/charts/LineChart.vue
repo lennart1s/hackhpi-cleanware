dc5b11b2c8d87728c9f8506cb1ab9f5f2b625ea5
lennart1s <sandbothe.lennart@gmail.com>
2022-03-20 13:00:11 +0100

    .

--- website/src/components/charts/LineChart.vue
+++ website/src/components/charts/LineChart.vue
@@ -3,24 +3,33 @@ import { Line } from 'vue-chartjs';
 
 export default {
   extends: Line,
+  data: () => ({
+    valuesA: [425, 413, 511, 538, 553, 588, 552, 430, 428],
+    valuesB: [310, 322, 341, 372, 374, 407, 410, 408, 410],
+  }),
   mounted() {
     this.renderChart({
       labels: ['-8h', '-7h', '-6h', '-5h', '-4h', '-3h', '-2h', '-1h', '-0h'],
       datasets: [
         {
           label: 'Total Energy Consumption',
-          data: [2, 5, 4, 8],
+          data: this.valuesA,
           backgroundColor: 'rgba(245, 90, 0, 0.3)',
         },
         {
           label: 'Reneawble Energy',
-          data: [1, 6, 4, 7],
+          data: this.valuesB,
           backgroundColor: 'rgba(32, 212, 41, 0.3)',
         },
       ],
     }, {
       responsive: false,
     });
+  },
+  methods: {
+    update() {
+
+    },
   },
 };
 </script>
--- website/src/store/modules/simulation.js
+++ website/src/store/modules/simulation.js
@@ -17,5 +17,15 @@ export default {
 
       console.log(data);
     },
+    getFakeData({ state }) {
+      let valuesA = [425, 413, 511, 502, 492, 490, 493, 430, 428];
+      let valuesB = [310, 322, 341, 372, 374, 407, 410, 408, 410];
+      for (let i = 0; i <= state.time; i += 1) {
+        valuesA = valuesA.unshift(0);
+        valuesB = valuesB.unshift(0);
+      }
+
+      return { valuesA, valuesB };
+    },
   },
 };
